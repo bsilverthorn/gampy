df1942f95f66a6d99a1773588ad19c12b525a00a
Bryan Silverthorn <bcs@cargo-cult.org>
2011-04-14 10:02:03 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+GAMpy
+=====
+
+Generalized additive models in Python. DO NOT USE. Still no more than a design
+sandbox.
+
---
+++ backfit.py
@@ -0,0 +1,12 @@
+import plac
+
+if __name__ == "__main__":
+    pass
+
+import numpy
+
+def main():
+    data = numpy.recfromtxt("data.txt", delimiter = ",")
+
+    print data
+
